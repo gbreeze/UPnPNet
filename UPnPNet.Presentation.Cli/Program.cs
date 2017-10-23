22f5770588b826e52ca1a8c25f6f38125c7ac981
Georg Perhofer <georg.perhofer@aimsoftware.com>
2017-10-23 17:41:14 +0200

    Program output change

--- UPnPNet.Presentation.Cli/Program.cs
+++ UPnPNet.Presentation.Cli/Program.cs
@@ -9,97 +9,105 @@ using UPnPNet.Services.AvTransport;
 
 namespace UPnPNet.Presentation.Cli
 {
-	public static class CollectionExtensions
-	{
-		public static void Foreach<T>(this ICollection<T> collection, Action<T> command)
-		{
-			foreach (T var in collection)
-			{
-				command(var);
-			}
-		}
-	}
+    public static class CollectionExtensions
+    {
+        public static void Foreach<T>(this ICollection<T> collection, Action<T> command)
+        {
+            foreach (T var in collection)
+            {
+                command(var);
+            }
+        }
+    }
 
-	public class Program
-	{
-		public static void Main()
-		{
-			UPnPDiscovery discovery = new UPnPDiscovery
-			{
-				SearchTarget = DiscoverySearchTargetFactory.ServiceTypeSearch("AVTransport", "1")
-			};
-			IList<UPnPDevice> devices = discovery.Search().Result;
+    public class Program
+    {
+        public static void Main()
+        {
+            UPnPDiscovery discovery = new UPnPDiscovery
+            {
+                //SearchTarget = DiscoverySearchTargetFactory.ServiceTypeSearch("AVTransport", "1")
+                SearchTarget = DiscoverySearchTargetFactory.AllSearch(),
+                WaitTimeInSeconds = 5
+            };
+            IList<UPnPDevice> devices = discovery.Search().Result;
 
-			UPnPServer server = new UPnPServer();
+            Console.WriteLine("Devices Found: " + devices.Count);
 
-			IList<UPnPDevice> sonosDevices =
-				devices.Where(x => x.Properties["friendlyName"].ToLower().Contains("sonos")).ToList();
-
-			IList<UPnPService> avServices = sonosDevices
-				.SelectMany(x => x.SubDevices)
-				.SelectMany(x => x.Services)
-				.Where(x => x.Type == "urn:schemas-upnp-org:service:AVTransport:1").ToList();
-
-			IList<AvTransportServiceControl> speakers = avServices.Select(x => new AvTransportServiceControl(x)).ToList();
+            devices.Foreach((i) => PrintDevice(i));
 
 
-			server.Start(24458);
+            //UPnPServer server = new UPnPServer();
 
-			speakers.Foreach(x =>
-			{
-				server.SubscribeToControl(x);
-				x.OnLastChangeEvent += (sender, args) => { Console.WriteLine("SOMETHING: " + args.TransportState); };
-			});
+            //IList<UPnPDevice> sonosDevices =
+            //	devices.Where(x => x.Properties["friendlyName"].ToLower().Contains("sonos")).ToList();
+
+            //IList<UPnPService> avServices = sonosDevices
+            //	.SelectMany(x => x.SubDevices)
+            //	.SelectMany(x => x.Services)
+            //	.Where(x => x.Type == "urn:schemas-upnp-org:service:AVTransport:1").ToList();
+
+            //IList<AvTransportServiceControl> speakers = avServices.Select(x => new AvTransportServiceControl(x)).ToList();
 
 
-			while (true)
-			{
-				ConsoleKeyInfo info = Console.ReadKey();
+            //server.Start(24458);
 
-				switch (info.Key)
-				{
-					case ConsoleKey.Q:
-						return;
-					case ConsoleKey.A:
-						speakers.Foreach(
-							x => x.SendAction("Play", new Dictionary<string, string>() {{"InstanceID", "0"}, {"Speed", "1"}}).Wait());
-						break;
-					case ConsoleKey.S:
-						speakers.Foreach(x => x.SendAction("Pause", new Dictionary<string, string>() {{"InstanceID", "0"}}).Wait());
-						break;
-				}
-			}
-		}
+            //speakers.Foreach(x =>
+            //{
+            //	server.SubscribeToControl(x);
+            //	x.OnLastChangeEvent += (sender, args) => { Console.WriteLine("SOMETHING: " + args.TransportState); };
+            //});
 
-		public static void PrintDevice(UPnPDevice device, int indentation = 0)
-		{
-			string identation = new string('\t', indentation);
 
-			Console.WriteLine(identation + "==" + device.Properties["friendlyName"] + "==");
+            //while (true)
+            //{
+            //	ConsoleKeyInfo info = Console.ReadKey();
 
-			Console.WriteLine(identation + "Properties:");
-			foreach (KeyValuePair<string, string> keyValuePair in device.Properties)
-			{
-				Console.WriteLine(identation + "\t - " + keyValuePair.Key + ": " + keyValuePair.Value);
-			}
+            //	switch (info.Key)
+            //	{
+            //		case ConsoleKey.Q:
+            //			return;
+            //		case ConsoleKey.A:
+            //			speakers.Foreach(
+            //				x => x.SendAction("Play", new Dictionary<string, string>() {{"InstanceID", "0"}, {"Speed", "1"}}).Wait());
+            //			break;
+            //		case ConsoleKey.S:
+            //			speakers.Foreach(x => x.SendAction("Pause", new Dictionary<string, string>() {{"InstanceID", "0"}}).Wait());
+            //			break;
+            //	}
+            //}
+            Console.ReadLine();
+        }
 
-			Console.WriteLine(identation + "Services:");
-			foreach (UPnPService service in device.Services)
-			{
-				Console.WriteLine(identation + "\t - " + service.Type);
-			}
+        public static void PrintDevice(UPnPDevice device, int indentation = 0)
+        {
+            string identation = new string('\t', indentation);
 
-			Console.WriteLine(identation + "Actions:");
-			foreach (string target in device.Targets)
-			{
-				Console.WriteLine(identation + "\t - " + target);
-			}
+            Console.WriteLine(identation + "==" + device.Properties["friendlyName"] + "==");
 
-			Console.WriteLine(identation + "SubDevices:");
-			foreach (UPnPDevice subDevice in device.SubDevices)
-			{
-				PrintDevice(subDevice, indentation + 1);
-			}
-		}
-	}
+            Console.WriteLine(identation + "Properties:");
+            foreach (KeyValuePair<string, string> keyValuePair in device.Properties)
+            {
+                Console.WriteLine(identation + "\t - " + keyValuePair.Key + ": " + keyValuePair.Value);
+            }
+
+            Console.WriteLine(identation + "Services:");
+            foreach (UPnPService service in device.Services)
+            {
+                Console.WriteLine(identation + "\t - " + service.Type);
+            }
+
+            Console.WriteLine(identation + "Actions:");
+            foreach (string target in device.Targets)
+            {
+                Console.WriteLine(identation + "\t - " + target);
+            }
+
+            Console.WriteLine(identation + "SubDevices:");
+            foreach (UPnPDevice subDevice in device.SubDevices)
+            {
+                PrintDevice(subDevice, indentation + 1);
+            }
+        }
+    }
 }
